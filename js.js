7946a40371112a09a7df256a6671fca62c5fcb8b
risqiboyevbobur <brisqiboyev@gmail.com>
2022-12-23 18:34:00 +0500

    vv

--- js.js
+++ js.js
@@ -1,10 +1,64 @@
+// const getItem = () =>{
+//     let inp = document.querySelector('input')
+// console.log(inp.value);
+
+
+
+// if(inp.value !== '' && !inp.value.includes(" ") ){
+//     let data = {
+//         name:inp.value
+//     }
+//     local.setItem('user-name', JSON.stringify(data))
+    
+//     let get_item = local.getItem('user-name')
+    
+//     get_item = JSON.parse(get_item)
+//     console.log(get_item);
+
+// }else{
+//     alert('iltimos bo\'sh joyni to\'ldiring' )
+// }
+
+
+
+
+// }
+// let local = localStorage
+// let head  = document.querySelector('h1')
+// head.innerHTML = JSON.parse(local['user-name']).name
+
+// const remowe = () =>{
+//     local.clear('user-name')
+// }
+
+
+////////////////////////////////////////////////////////
 const getItem = () =>{
     let inp = document.querySelector('input')
 console.log(inp.value);
 
-    let local = localStorage
+if(inp.value !== '' && !inp.value.includes(" ") ){
+    let data = {
+        name:inp.value
+    }
+    local.setItem('user-name', JSON.stringify(data))
+    
+    let get_item = local.getItem('user-name')
+    
+    get_item = JSON.parse(get_item)
 
-local.setItem('user-name', inp.value)
-let get_item = local.getItem('user-name')
-console.log(get_item);
+alert(`your email is  ---  ${inp.value}  --- `)
+} 
+else{
+    alert('iltimos bo\'sh joyni to\'ldiring' )
 }
+
+}
+let local = localStorage
+let head  = document.querySelector('h1')
+head.innerHTML = JSON.parse(local['user-name']).name
+
+const remowe = () =>{
+    local.clear('user-name')
+}
+
